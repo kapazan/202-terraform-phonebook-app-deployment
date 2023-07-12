1e1768d295be8194e4022b3cd9d103b41199b010
kapazan <kapazanrizgar@gmail.com>
2023-07-12 20:16:35 +0200

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "kapazan"
+}
+
+variable "git-token" {
+  default = "****"
+}
+
+variable "key-name" {
+  default = "****"
+}
+
+variable "hosted-zone" {
+  default = "gottogo.click."
+}

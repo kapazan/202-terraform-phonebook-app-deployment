59190d427309915b12b5f2bceaaffd3354b0ee22
kapazan <kapazanrizgar@gmail.com>
2023-08-15 17:32:03 +0200

    source code added

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -4,6 +4,7 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/<account-name>/phonebook.git
+TOKEN=${user-data-git-token}
+USER=${user-data-git-name}
+cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
